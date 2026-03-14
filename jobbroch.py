b5880e76f900535835483d237bc7e9762c44c986
Sophie-Muchiri12 <muthonisophie12@gmail.com>
2026-03-15 00:00:34 +0300

    get user link function

--- jobbroch.py
+++ jobbroch.py
@@ -7,15 +7,16 @@ from bs4 import BeautifulSoup
 from IPython.display import Markdown, display, update_display
 from openai import OpenAI
 import json
+from linksite import Website
 
 # Retrieving keys from .env
 load_dotenv(override=True)
-api_key = os.get('GROQ_API_KEY')
+api_key = os.getenv('GROQ_API_KEY')
 
 #Instantiate openai object with key and baseurl
 openai = OpenAI(
     api_key=api_key,
-    baseurl='https://api.groq.com/openai/v1'
+    base_url='https://api.groq.com/openai/v1'
 )
 
 #set up model to use
@@ -37,8 +38,8 @@ WORK FLOW:
     * format the job seeking briefing as mark down
 
 ## Functions & variables to used
-1 link_system_prompt - direct llm to scrape links related to job seekers
-2 user_link_prompt - provide the llm the website url and direct it to do the same
+1 link_system_prompt - direct llm to scrape links related to job seekers [ :) ]
+2 user_link_prompt function - provide the llm the website url and direct it to do the same
 3 get_link function - will use the link_system_prompt and user_link_prompt
 4 get_content_link function - will fetch the content of site and all links using get_link function
 5 job_brief_system_prompt - direct llm to create a job seeking briefing based on the url that the user will provide
@@ -47,4 +48,70 @@ WORK FLOW:
 
 
 '''
+# 1. link_system prompt
 
+link_system_prompt = """
+You are an assistant that analyzes a list of website links.
+
+Your goal is to identify links that would be useful for a JOB SEEKER researching a company.
+
+Select links related to:
+- About the company
+- Careers / Jobs
+- Team
+- Culture
+- Engineering or Technology
+- Blog or News
+- Mission / Values
+
+Ignore links related to:
+- Privacy policy
+- Terms of service
+- Cookie policy
+- Contact forms
+- Social media (Twitter, LinkedIn, Facebook, Instagram)
+- Login / Sign up pages
+
+Return your answer as STRICT JSON.
+
+Each item must contain:
+"type" – the category of the page
+"url" – the FULL https URL of the page
+
+Example output:
+
+{
+  "links": [
+    {"type": "about", "url": "https://example.com/about"},
+    {"type": "careers", "url": "https://example.com/careers"},
+    {"type": "engineering", "url": "https://example.com/engineering-blog"}
+  ]
+}
+"""
+
+# 2 user_link_prompt function
+
+def get_user_link_prompt(url):
+    web = Website(url)
+    user_prompt = f"You are analyzing a company's website: {web}"
+    user_prompt += '''
+    Your task is to identify which links would be useful for a JOB SEEKER researching this company.
+
+Select links related to:
+- About
+- Careers / Jobs
+- Team
+- Culture
+- Engineering / Technology
+- Blog / News
+- Mission / Values
+
+Here are the links found on the website:
+    '''
+    user_prompt += "\n".join(web.links)
+    return user_prompt
+
+
+print(get_user_link_prompt("https://huggingface.co/"))
+
+    
